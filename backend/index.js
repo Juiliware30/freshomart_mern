067b589c5c654f5498084e5bd2097b81414bb186
juili <juiware30@gmail.com>
2026-04-29 15:32:54 +0530

    Fixed :issues

--- backend/index.js
+++ backend/index.js
@@ -71,6 +71,10 @@ app.use("/api/payment", paymentRoutes);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+  });
+}
+
+export default app;
